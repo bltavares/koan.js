8d40139cf120c8726a2a3bc723b256a3946d67e6
Bruno Tavares <bruno.exz@gmail.com>
2014-04-10 21:16:53 -0300

    Add numbers sections

--- spec/koan_spec.js
+++ spec/koan_spec.js
@@ -1,6 +1,27 @@
 var __ = 'Fill me in';
 
 describe('koan.js', function () {
+  describe('Numbers', function () {
+    it('should create literals', function () {
+      expect(1).toBe(__);
+    });
+
+    it('should add numbers', function () {
+      expect(1 + 1).toBe(__);
+    });
+
+    it('should subtract numbers', function () {
+      expect(1 - 1).toBe(__);
+    });
+
+    it('should multiply numbers', function () {
+      expect(2 * 2).toBe(__);
+    });
+
+    it('should divide numbers', function () {
+      expect(2 / 2).toBe(__);
+    });
+  });
 
   describe('Strings', function () {
     describe('literal', function () {
