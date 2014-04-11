e35ab8e3b20a98b59d99f0a3fbce1a89aa286013
Bruno Tavares <bruno.exz@gmail.com>
2014-04-10 21:29:12 -0300

    Start introducing array tests

--- spec/koan_spec.js
+++ spec/koan_spec.js
@@ -136,4 +136,25 @@ describe('koan.js', function () {
       });
     });
   });
+
+  describe('Arrays', function () {
+    describe('literals', function () {
+      it('should create literals', function () {
+        expect([1, 2, 3]).toEqual(__);
+      });
+
+      it('should access indexes', function () {
+        var array = [1, 2, 3];
+
+        expect(array[0]).toEqual(__);
+      });
+
+      it('should set values on indexes', function () {
+        var array = [1, 2, 3];
+        array[3] = 4;
+
+        expect(array).toEqual(__);
+      });
+    });
+  });
 });
