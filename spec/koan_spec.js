5d499f8840ed3b3a45a5f94d9f1dd8ec7a0f6877
Bruno Tavares <bruno.exz@gmail.com>
2014-04-10 21:00:45 -0300

    Migrate FILL_IN to __

--- spec/koan_spec.js
+++ spec/koan_spec.js
@@ -1,36 +1,36 @@
-var FILL_IN = 'Fill me in';
+var __ = 'Fill me in';
 
 describe('koan.js', function () {
   describe('Strings', function () {
 
     it('should create Strings', function () {
-      expect('a String').toBe(FILL_IN);
-      expect(new String('a String')).toBe(FILL_IN);
+      expect('a String').toBe(__);
+      expect(new String('a String')).toBe(__);
     });
 
     it('should concatenate Strings', function () {
-      expect('a ' + 'String').toBe(FILL_IN);
-      expect(new String('a ') + 'String').toBe(FILL_IN);
+      expect('a ' + 'String').toBe(__);
+      expect(new String('a ') + 'String').toBe(__);
     });
 
     it('should convert an Number to String', function () {
-      expect((1).toString()).toBe(FILL_IN);
-      expect(new String(1)).toBe(FILL_IN);
+      expect((1).toString()).toBe(__);
+      expect(new String(1)).toBe(__);
     });
 
     it('should convert an Array to String', function () {
-      expect([1, 2, 3].toString()).toBe(FILL_IN);
-      expect(new String([1, 2, 3])).toBe(FILL_IN);
+      expect([1, 2, 3].toString()).toBe(__);
+      expect(new String([1, 2, 3])).toBe(__);
     });
 
     it('should convert an Object to String', function () {
-      expect({ 'abc': 1, '2': 'banana'}.toString()).toBe(FILL_IN);
-      expect(new String({ 'abc': 1, '2': 'banana'})).toBe(FILL_IN);
+      expect({ 'abc': 1, '2': 'banana'}.toString()).toBe(__);
+      expect(new String({ 'abc': 1, '2': 'banana'})).toBe(__);
     });
 
     it('should have a type', function () {
-      expect(typeof '').toBe(FILL_IN);
-      expect(typeof new String()).toBe(FILL_IN);
+      expect(typeof '').toBe(__);
+      expect(typeof new String()).toBe(__);
     });
 
   });
